0a6d045e6ebf91486ee26820540cc6fb963e44f2
Sebastian DL <delucasd@gmail.com>
2016-04-06 15:28:58 -0300

    add call to get product's stock

--- lib/stoneacre_hq/product.rb
+++ lib/stoneacre_hq/product.rb
@@ -1,11 +1,13 @@
 module StoneacreHq
   class Product < StoneacreHq::Base
-    
-    ##
-    # 
-    #
+
+    def get_stock(club_id, sku)
+      @conn.post PRODUCT_URL+'/get_stock', { api_key: StoneacreHq.config[:api_key], club_id: club_id, sku: sku}
+    end
+   
     def find_all_by_product_skus(club_id, skus = [])
       @conn.post PRODUCT_URL+'/get_list_of_stock', { api_key: StoneacreHq.config[:api_key], club_id: club_id, sku: skus }
     end
+
   end
 end
